afa108c33ef96a0a2abd9b42a709556b3fbe0074
unknown <shailesh0g@gmail.com>
2018-09-23 08:37:17 +0000

    Static code check warnings fixes

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,8 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**COmments for Method */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
