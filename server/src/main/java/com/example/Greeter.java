cb20da311eb5d3c9eeea0e1df883ce89bb9cb9bd
unknown <shailesh0g@gmail.com>
2018-09-30 08:46:39 +0000

    3

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,10 +12,7 @@ public class Greeter {
 
   }
 
-  /**
-  * @param someone the name of a person
-  * @  greeting string
-  */
+ 
     public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
